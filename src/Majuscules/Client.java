ca2025b3563f7d9ce586372420d5f315b965c191
Sergio Sanchis <sergiosanchis@hotmail.com>
2015-12-16 20:31:51 +0100

    Modificacions client

--- src/Majuscules/Client.java
+++ src/Majuscules/Client.java
@@ -1,12 +1,10 @@
 
-package tcpclient;
+package Majuscules;
 import java.io.*;
 import java.net.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
-public class TCPClient {
+public class Client {
     public static void main(String[] args) {
     	
     	int PORT = 7000;
