5701efc213de3725fd6b0f5d87fa874488fc7151
fenderslender <jolium@hotmail.com>
2015-12-16 20:28:53 +0100

    Update Client.java

--- src/Majuscules/Client.java
+++ src/Majuscules/Client.java
@@ -1,38 +1,56 @@
-package Majuscules;
 
+package tcpclient;
 import java.io.*;
 import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
-public class Client {
-    public static void main(String[] args) throws IOException {
+public class TCPClient {
+    public static void main(String[] args) {
     	
-    	final int PORT = 7000;
+    	int PORT = 7000;
+    	String IP = "10.2.1.179";
+    	int LocalPORT = 8000;
         
         BufferedReader EntradaTeclat = new BufferedReader(new InputStreamReader(System.in));
         
         DataOutputStream SortidaAlServidor = null;
         BufferedReader EntradaDesdeServidor = null;
         
-        String frase;
+        String frase; 
+        String benbingudaServer;
         String fraseModificada;
         
         Socket clientSocket;
-        clientSocket = new Socket("127.0.0.1", PORT);
+        try {
+            clientSocket = new Socket(IP, PORT, null , LocalPORT);
+            //clientSocket = new Socket("10.2.1.179", PORT, InetAddress.getByName("localhost"),7001);
+            SortidaAlServidor = new DataOutputStream(clientSocket.getOutputStream());
+            EntradaDesdeServidor = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            benbingudaServer = EntradaDesdeServidor.readLine();
+            System.out.println(benbingudaServer);
+
+            System.out.println("Donam una frase");
+            frase = EntradaTeclat.readLine();
+
+            SortidaAlServidor.writeBytes(frase + "\n");
+
+            fraseModificada = EntradaDesdeServidor.readLine();
+
+            System.out.println("FROM SERVER " + fraseModificada);
+
+            clientSocket.close();
+            System.out.println("Connexio tancada");
         
-        SortidaAlServidor = new DataOutputStream(clientSocket.getOutputStream());
-        EntradaDesdeServidor = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         
-        System.out.println("Escriu la grase que vols enviar al servidor: ");
-        frase = EntradaTeclat.readLine();
+        } catch (IOException ex) {
+            //Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("No es pot establir connexió");
+        }
         
-        SortidaAlServidor.writeBytes(frase + "\n");
         
-        fraseModificada = EntradaDesdeServidor.readLine();
-        
-        System.out.println("FROM SERVER " + fraseModificada);
-        
-        clientSocket.close();
         
     }
     
